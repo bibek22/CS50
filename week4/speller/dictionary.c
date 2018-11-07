669808f378f9c27088017abaf3e1696d8cb95f33
Bibek Gautam <forgot.bg@gmail.com>
2018-11-07 19:24:12 +0545

    first commit

---
+++ week4/speller/dictionary.c
@@ -0,0 +1,42 @@
+// Implements a dictionary's functionality
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "dictionary.h"
+
+// Returns true if word is in dictionary else false
+bool check(const char *word)
+{
+    // TODO
+    return false;
+}
+
+// Loads dictionary into memory, returning true if successful else false
+bool load(const char *dictionary)
+{
+    char c;
+    FILE *handle = fopen(dictionary, "r");
+    while ((c = fgetc(handle)) != EOF){
+        while (c != '\n'){
+        
+        }
+    }
+        
+
+    // TODO
+    return false;
+}
+
+// Returns number of words in dictionary if loaded else 0 if not yet loaded
+unsigned int size(void)
+{
+    // TODO
+    return 0;
+}
+
+// Unloads dictionary from memory, returning true if successful else false
+bool unload(void)
+{
+    // TODO
+    return false;
+}
